48574b90abcacc4f2729d55b0aaa81744dd96b97
KirillDolbnya <kirilldolbnya6@gmail.com>
2023-05-14 19:28:41 +0300

    added check for id and role user

--- App/views/security.php
+++ App/views/security.php
@@ -5,7 +5,7 @@ if( !session_id() ) {
 }
 
 
-$this->layout('template',['title' => 'Edit']);
+$this->layout('template', ['title' => 'Security']);
 
 use App\views\services\CheckLogin;
 use Tamtamchik\SimpleFlash\Flash;
@@ -14,12 +14,12 @@ use App\views\services\CheckingRoles;
 $checkLogin = new CheckLogin();
 $checkLogin->checkLogin();
 
-$idUser = $GLOBALS['vars']['id'];
-$idUser = (int)$idUser;
-
 $checkLogin = new CheckLogin();
 $checkLogin->checkLogin();
 $checkRole = new CheckingRoles();
+
+$idUser = $GLOBALS['vars']['id'];
+$idUser = (int)$idUser;
 
 if ($_SESSION['auth_user_id'] !== $idUser && !$checkRole->checkrole($_SESSION['auth_user_id'])){
     \flash()->error('Можно редактировать только свой профиль');
@@ -29,7 +29,7 @@ if ($_SESSION['auth_user_id'] !== $idUser && !$checkRole->checkrole($_SESSION['a
 
 ?>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-        <a class="navbar-brand d-flex align-items-center fw-500" href="/users"><img alt="logo" class="d-inline-block align-top mr-2" src="/App/views/assets/img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
+        <a class="navbar-brand d-flex align-items-center fw-500" href="users.php"><img alt="logo" class="d-inline-block align-top mr-2" src="/App/views/assets/img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarColor02">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
@@ -38,10 +38,10 @@ if ($_SESSION['auth_user_id'] !== $idUser && !$checkRole->checkrole($_SESSION['a
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/login">Войти</a>
+                    <a class="nav-link" href="page_login.php">Войти</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/logout">Выйти</a>
+                    <a class="nav-link" href="#">Выйти</a>
                 </li>
             </ul>
         </div>
@@ -49,50 +49,51 @@ if ($_SESSION['auth_user_id'] !== $idUser && !$checkRole->checkrole($_SESSION['a
     <main id="js-page-content" role="main" class="page-content mt-3">
         <div class="subheader">
             <h1 class="subheader-title">
-                <i class='subheader-icon fal fa-plus-circle'></i> Редактировать
+                <i class='subheader-icon fal fa-lock'></i> Безопасность
             </h1>
             <div>
                 <?php echo flash()->display(); ?>
             </div>
         </div>
-        <form action="/editService" method="post">
+        <form action="/securityService" method="post">
             <div class="row">
                 <div class="col-xl-6">
                     <div id="panel-1" class="panel">
                         <div class="panel-container">
                             <div class="panel-hdr">
-                                <h2>Общая информация</h2>
+                                <h2>Обновление эл. адреса и пароля</h2>
                             </div>
                             <div class="panel-content">
-                                <!-- username -->
+                                <!-- email -->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Имя</label>
-                                    <input name="username" type="text" id="simpleinput" class="form-control" >
+                                    <label class="form-label" for="simpleinput">Email</label>
+                                    <input name="email" type="text" id="simpleinput" class="form-control">
                                 </div>
 
-                                <!-- title -->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Место работы</label>
-                                    <input name="work" type="text" id="simpleinput" class="form-control">
+                                    <label class="form-label" for="simpleinput">Пароль установлен на данный момент</label>
+                                    <input name="OldPassword" type="password" id="simpleinput" class="form-control">
                                 </div>
 
-                                <!-- tel -->
+                                <!-- password -->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Номер телефона</label>
-                                    <input name="tel" type="number" id="simpleinput" class="form-control">
+                                    <label class="form-label" for="simpleinput">Новый Пароль</label>
+                                    <input name="NewPassword" type="password" id="simpleinput" class="form-control">
                                 </div>
 
-                                <!-- address -->
+                                <!-- password confirmation-->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Адрес</label>
-                                    <input name="address" type="text" id="simpleinput" class="form-control">
+                                    <label class="form-label" for="simpleinput">Подтверждение пароля</label>
+                                    <input name="NewAgainPassword" type="password" id="simpleinput" class="form-control">
                                 </div>
-                                <input type="hidden" name="id" value="<?php $idUser ?>">
+
+
                                 <div class="col-md-12 mt-3 d-flex flex-row-reverse">
-                                    <button type="submit" class="btn btn-warning">Редактировать</button>
+                                    <button class="btn btn-warning">Изменить</button>
                                 </div>
                             </div>
                         </div>
+                        
                     </div>
                 </div>
             </div>
